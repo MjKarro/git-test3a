d137c9eea16f089685baa5263e92cf9e1b3203d6
Guz <guz@123.com>
2022-09-01 20:20:06 +0800

    second commit

--- src/main/java/git/gitTest.java
+++ src/main/java/git/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("****");
+        System.out.println("222222222");
     }
 }
